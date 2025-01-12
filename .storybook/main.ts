66b4ac3dc9d10f58d5ae435a58d351cb8fa68925
Tomasz Ducin <tomasz@ducin.dev>
2025-01-12 15:51:49 +0100

    update

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,6 +8,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@chromatic-com/storybook",
+    "@storybook/addon-a11y"
   ],
   framework: {
     name: "@storybook/react-vite",
